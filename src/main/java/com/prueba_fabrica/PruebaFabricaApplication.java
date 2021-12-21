c4fca04cb38433e61eb96801d1f09f1cb175c0bd
villamizar21 <arleyvillamizar@wposs.com>
2021-12-21 08:42:08 -0500

    Se agregan dependencias

---
+++ src/main/java/com/prueba_fabrica/PruebaFabricaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba_fabrica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaFabricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaFabricaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba_fabrica/PruebaFabricaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba_fabrica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaFabricaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
